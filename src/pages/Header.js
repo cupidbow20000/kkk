cce2521997ce0c5b47de720729f76e79dbc7e722
Russell1130 <devlord0625@gmail.com>
2023-02-21 05:04:04 -0500

    PAOXY

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -11,12 +11,21 @@ import Grid from '@mui/material/Grid';
 export default function Header() {
 
     const [anchorEl, setAnchorEl] = React.useState("none");
-    const open = Boolean(anchorEl);
+    const [anchorEl2, setAnchorEl2] = React.useState("none");
+    const open = anchorEl, open2 = anchorEl2;
     const handleClick = (event) => {
       setAnchorEl("block");
+      setAnchorEl2("none");
+    };
+    const handleClick2 = (event) => {
+      setAnchorEl("none");
+      setAnchorEl2("block");
     };
     const handleClose = () => {
       setAnchorEl("none");
+    };
+    const handleClose2 = () => {
+      setAnchorEl2("none");
     };
 
     const Item = styled(Paper)(({ theme }) => ({
@@ -32,61 +41,117 @@ export default function Header() {
           <a href="" style={{paddingLeft:"10%"}}>
             <img src="../paxos-logo.svg" title="paxos" alt="paxos" loading="lazy" />
           </a>
-            <Button
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onMouseEnter={handleClick}
-         style={{marginLeft:"30%"}}
-      >
-        Products
-      </Button>
-            
-              <Box sx={{ flexGrow: 1 }} id="basic-menu"
-                open={open}
-                display={anchorEl}
-                onMouseOut={handleClose}
-                style={{marginLeft:"10%", marginRight:"10%",  padding:"10px", border:"ridge" }}
-                >
-                <Grid container spacing={{ xs: 5, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                  <Grid item xs={4} sm={4} md={4} key={0}>
-                    <Item><p className="myupper">CRYPTO FOR ENTERPRISE</p></Item>
-                  </Grid>
-                  <Grid item xs={4} sm={4} md={4} key={0}>
-                    <Item><p className="myupper">CRYPTO FOR INVESTORS</p></Item>
-                  </Grid>
-                  <Grid item xs={4} sm={4} md={4} key={0}>
-                    <Item><p className="myupper">SETTLEMENT FOR INSTITUTIONS</p></Item>
-                  </Grid>
-                  <Grid item xs={4} sm={4} md={4} key={0}>
-                    <Item>Crypto Brokerage<br/>Build your crypto solution</Item>
-                  </Grid>
-                  <Grid item xs={4} sm={4} md={4} key={0}>
-                    <Item>itBit<br/>Crypto exchange</Item>
-                  </Grid>
-                  <Grid item xs={4} sm={4} md={4} key={0}>
-                    <Item>Securities<br/>Equities settlement for broker-dealers</Item>
-                  </Grid>
-                  <Grid item xs={4} sm={4} md={4} key={0}>
-                    <Item>Stablecoin and Payments<br/>Blockchain-enabled money movement</Item>
-                  </Grid>
-                  <Grid item xs={4} sm={4} md={4} key={0}>
-                    <Item>BUSD<br/>Regulated digital dollar</Item>
-                  </Grid>
-                  <Grid item xs={4} sm={4} md={4} key={0}>
-                    <Item>Commodities<br/>Precious metals settlement</Item>
-                  </Grid>
-                  <Grid item xs={4} sm={4} md={4} key={1}></Grid>
- 
-                  <Grid item xs={4} sm={4} md={4} key={0}></Grid>
-                  <Grid item xs={4} sm={4} md={4} key={0}></Grid>
-                  <Grid item xs={4} sm={4} md={4} key={0}>
-                    <Item>PAXG<br/>Tokenized gold</Item>
-                  </Grid>
-                  <Grid item xs={4} sm={4} md={4} key={0}></Grid>
-                </Grid>
-              </Box>
+          <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onMouseEnter={handleClick}
+            style={{marginLeft:"30%"}}
+          >
+            Products
+          </Button>
+          <Button
+            id="basic-button2"
+            aria-controls={open2 ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open2 ? 'true' : undefined}
+            onMouseEnter={handleClick2}
+            style={{marginLeft:"5%"}}
+          >
+            Company
+          </Button>
+          <Box sx={{ flexGrow: 1 }} id="basic-menu"
+            open={open}
+            display={anchorEl}
+            onMouseLeave={handleClose}
+            style={{marginLeft:"10%", marginRight:"10%",  padding:"10px", border:"ridge" }}
+            >
+            <Grid container spacing={{ xs: 5, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item><p className="myupper">CRYPTO FOR ENTERPRISE</p></Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item><p className="myupper">CRYPTO FOR INVESTORS</p></Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item><p className="myupper">SETTLEMENT FOR INSTITUTIONS</p></Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>Crypto Brokerage<br/>Build your crypto solution</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>itBit<br/>Crypto exchange</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>Securities<br/>Equities settlement for broker-dealers</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>Stablecoin and Payments<br/>Blockchain-enabled money movement</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>BUSD<br/>Regulated digital dollar</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>Commodities<br/>Precious metals settlement</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}></Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>USDP<br/>Regulated digital dollar</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}></Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}></Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>PAXG<br/>Tokenized gold</Item>
+              </Grid>
+            </Grid>
+          </Box>
+        
+          <Box sx={{ flexGrow: 1 }} id="basic-menu"
+            open={open2}
+            display={anchorEl2}
+            onMouseLeave={handleClose2}
+            style={{ border:"ridge" }}
+            >
+            <Grid container spacing={{ xs: 5, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item><p className="myupper">CRYPTO FOR ENTERPRISE</p></Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item><p className="myupper">CRYPTO FOR INVESTORS</p></Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item><p className="myupper">SETTLEMENT FOR INSTITUTIONS</p></Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>Crypto Brokerage<br/>Build your crypto solution</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>itBit<br/>Crypto exchange</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>Securities<br/>Equities settlement for broker-dealers</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>Stablecoin and Payments<br/>Blockchain-enabled money movement</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>BUSD<br/>Regulated digital dollar</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>Commodities<br/>Precious metals settlement</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}></Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>USDP<br/>Regulated digital dollar</Item>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}></Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}></Grid>
+              <Grid item xs={4} sm={4} md={4} key={0}>
+                <Item>PAXG<br/>Tokenized gold</Item>
+              </Grid>
+            </Grid>
+          </Box>
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,55 @@
 import '../css/home.css';
+import * as React from 'react';
 
 function Home() {
+
+    const [ind, setInd] = React.useState(1);
+
+    const showSlides = (n) => {
+        let i, slideIndex=ind;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+        //slides[slideIndex-1].setStyle("display:block");
+        //dots[slideIndex-1].className += " active";
+        console.log(slides, "-----", slideIndex); 
+    }
+
+    const plusSlides = (val) => {
+        console.log("-----onplus");
+        showSlides(ind+val);
+    }
+
+    const currentSlide = (val) => {
+        showSlides(ind);
+    }
+
     return (
         <div className="myHome">
-            This is Home!
+            <div className="slideshow-container">
+                <div className="mySlides fade">
+                <div className="numbertext">1 / 3</div>
+                <img src="../img1.jpg" style={{width:"100%"}} />
+                <div className="text">Caption Text</div>
+                </div>
+
+                <a className="prev" onClick={plusSlides(-1)}>&#10094;</a>
+                <a className="next" onClick={plusSlides(1)}>&#10095;</a>
+                </div>
+                <br/>
+
+                <div style={{textAlign:"center"}}>
+                <span className="dot" onClick={currentSlide(1)}></span>
+                <span className="dot" onClick={currentSlide(2)}></span>
+                <span className="dot" onClick={currentSlide(3)}></span>
+                </div>
         </div>
     );
 }
