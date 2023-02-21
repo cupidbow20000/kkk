ee785a5a7ab62ec60f0afdf6bf709f622a5194db
Russell1130 <devlord0625@gmail.com>
2023-02-21 06:03:30 -0500

    PAOXY

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -65,7 +65,7 @@ export default function Header() {
             open={open}
             display={anchorEl}
             onMouseLeave={handleClose}
-            style={{marginLeft:"10%", marginRight:"10%",  padding:"10px", border:"ridge" }}
+            style={{marginLeft:"10%", marginRight:"10%",  padding:"10px", border:"ridge" , backgroundColor:"white"}}
             >
             <Grid container spacing={{ xs: 5, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
               <Grid item xs={4} sm={4} md={4} key={0}>
@@ -111,7 +111,7 @@ export default function Header() {
             open={open2}
             display={anchorEl2}
             onMouseLeave={handleClose2}
-            style={{ border:"ridge" }}
+            style={{ border:"ridge" , backgroundColor:"white"}}
             >
             <Grid container spacing={{ xs: 5, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
               <Grid item xs={4} sm={4} md={4} key={0}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,49 +6,50 @@ function Home() {
     const [ind, setInd] = React.useState(1);
 
     const showSlides = (n) => {
-        let i, slideIndex=ind;
-        let slides = document.getElementsByClassName("mySlides");
-        let dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-        }
-        //slides[slideIndex-1].setStyle("display:block");
-        //dots[slideIndex-1].className += " active";
-        console.log(slides, "-----", slideIndex); 
+        console.log(ind);
+        let i, slideIndex;
     }
 
-    const plusSlides = (val) => {
-        console.log("-----onplus");
-        showSlides(ind+val);
+    const nextSlide = () => {
+        console.log("next");
+        if(ind!=5) setInd(ind+1);
+        else setInd(1);
+        showSlides(ind+1);
+    }
+
+    const prevSlide = () => {
+        console.log("prev");
+        if(ind!=1) setInd(ind-1);
+        else setInd(5);
+        showSlides(ind-1);
     }
 
     const currentSlide = (val) => {
+        console.log(val);
+        setInd(val);
         showSlides(ind);
     }
 
     return (
-        <div className="myHome">
+        <div className="myHome" style={{position:"absolute", zIndex:"-1"}}>
             <div className="slideshow-container">
                 <div className="mySlides fade">
                 <div className="numbertext">1 / 3</div>
-                <img src="../img1.jpg" style={{width:"100%"}} />
+                <img src={"../img"+ind+".png"} style={{width:"100%"}} />
                 <div className="text">Caption Text</div>
                 </div>
 
-                <a className="prev" onClick={plusSlides(-1)}>&#10094;</a>
-                <a className="next" onClick={plusSlides(1)}>&#10095;</a>
+                <a className="prev" onClick={prevSlide.bind()}>&#10094;</a>
+                <a className="next" onClick={nextSlide.bind()}>&#10095;</a>
                 </div>
                 <br/>
 
                 <div style={{textAlign:"center"}}>
-                <span className="dot" onClick={currentSlide(1)}></span>
-                <span className="dot" onClick={currentSlide(2)}></span>
-                <span className="dot" onClick={currentSlide(3)}></span>
+                <span className="dot" onClick={currentSlide.bind(this, 1)}></span>
+                <span className="dot" onClick={currentSlide.bind(this, 2)}></span>
+                <span className="dot" onClick={currentSlide.bind(this, 3)}></span>
+                <span className="dot" onClick={currentSlide.bind(this, 4)}></span>
+                <span className="dot" onClick={currentSlide.bind(this, 5)}></span>
                 </div>
         </div>
     );
